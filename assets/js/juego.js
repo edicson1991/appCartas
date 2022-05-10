5184877a1a9d90554ee296ca65e1c00fbcc828b8
edicson1991 <52640992+edicson1991@users.noreply.github.com>
2022-05-09 22:27:53 -0500

    primer commit

---
+++ assets/js/juego.js
@@ -0,0 +1,30 @@
+//** 2C = Two of clubs (Tréboles) */
+//** 2D = Two of Diaminds(Tréboles) */
+//** 2H = Two of Hearts (Tréboles) */
+//** 2S = Two of Spades (Tréboles) */
+
+let deck         = [];
+const tipos      = ['C','D','H','S'];
+const especiales = ['A','J','Q','K'];
+
+
+const crearDeck = () => {
+    for (let i = 2; i <= 10; i++){
+        for(let tipo of tipos){
+            deck.push(i + tipo);
+        }
+    }   
+    for(let tipo of tipos){
+        for(let esp of especiales){
+            deck.push(esp + tipo);
+        }
+    }
+
+    console.log(deck);
+    deck = _.shuffle(deck);
+    console.log(deck);  
+
+}
+
+
+crearDeck();
